d454c5914752fc9c43ddf195685d9dd0ae5b6269
NguyenThiThu-Ha <75465048+NguyenThiThu-Ha@users.noreply.github.com>
2020-12-15 15:29:53 +0700

    Update connection.php

--- php_3/connection.php
+++ php_3/connection.php
@@ -1,4 +1,39 @@
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE php4";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+//    echo "Database created successfully<br>";
+    try {
+        // sql to create table
+        $sql = "CREATE TABLE php4.data (
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `title` VARCHAR(100) NULL ,
+    `description` TEXT NULL ,
+    `image` VARCHAR(200) NULL ,
+    `status` TEXT NULL ,
+    `create_at` DATETIME NULL ,
+    `update_at` DATETIME NULL
+      )";
+
+        // use exec() because no results are returned
+    $conn->exec($sql);
+//    echo " successfully";
+        
+    } catch (PDOException $e) {
+//        echo $sql . "<br>" . $e->getMessage();
+    }
+} catch (PDOException $e) {
+//    echo $sql . "<br>" . $e->getMessage();
+}
 class DB
 {
     private static $instance = NULl;
@@ -14,3 +49,4 @@ class DB
       return self::$instance;
     }
 }
+
